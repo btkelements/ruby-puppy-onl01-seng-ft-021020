7ed9eec8cbb9324f695edf82e6b0bd18f24bb950
Aziz Nasirov <nasirovaziz@yahoo.com>
2020-02-24 00:09:52 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,22 +1,16 @@
 class Dog
-
-  attr_accessor :name
-
   @@all = []
-
+  
+  attr_accessor :name
+  
   def initialize(name)
     @name = name
-    @@all << self
+    save
   end
-
+  
+  def self.all
+    @all
+  end
+  
   def self.print_all
-    @@all.each do |dog|
-      puts dog.name
-    end
-  end
-
-  def self.clear_all
-    @@all.clear
-  end
-
-end
+    @@all.each do |
