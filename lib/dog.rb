65d3520de67e64aa5c6e49d8b1316ee029083884
Aziz Nasirov <nasirovaziz@yahoo.com>
2020-02-24 00:10:58 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -15,3 +15,14 @@ class Dog
   def self.print_all
     @@all.each do |dog|
       puts dog.name
+    end
+  end
+  
+  def save
+    @@all << self
+  end
+  
+  def self.clear_all
+    @@all.clear
+  end
+end
